e5e7c31a7731cb3e789a2116b37a747853c50b59
dansh12 <sbhti123@gmail.com>
2022-10-02 01:49:06 +0300

    mobile appdate

--- src/app/main-components/navigation/navigation.component.ts
+++ src/app/main-components/navigation/navigation.component.ts
@@ -1,20 +1,33 @@
-import { Component, ElementRef, OnInit, Pipe } from '@angular/core';
+import { Component, ElementRef, OnInit, Pipe, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 import { PagesListService } from '../../services/pages-list/pages-list.service';
-
+import {BreakpointObserver}from '@angular/cdk/layout'; 
 
 @Component({
   selector: 'app-pages',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.sass']
+  styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
   pages = this.pagesListService.getPages();
   searchArticleTitle = '';
   selectedPage=0;
 
-  constructor(private pagesListService: PagesListService) { }
+  constructor(private pagesListService: PagesListService, private sideNavObserver: BreakpointObserver) { }
 
   ngOnInit() {}
+
+  ngAfterViewInit(){
+    this.sideNavObserver.observe(['(max-width:750px)']).subscribe(state =>{
+      if(state.matches){
+        this.sidenav.mode ='over';
+        this.sidenav.close();
+      }else{
+        this.sidenav.mode='side';
+        this.sidenav.open();
+      }})
+  }
 
   buttonClick(pageName: string) {
     this.selectedPage= this.pages.findIndex(page=> page["title"] == pageName);
--- src/app/pipes/search-article.pipe.ts
+++ src/app/pipes/search-article.pipe.ts
@@ -1,11 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Page } from '../interfaces/page';
 
 @Pipe({
   name: 'searchArticle',
   pure: false
 })
 export class SearchArticlePipe implements PipeTransform {
-  transform(value: any, filterName: string, attribute: string){
-    return value.filter((item:any)=> {return !item[attribute].toLowerCase().indexOf(filterName.toLowerCase())})
+  transform(value: Array<Page>, filterName: string, attribute: string){
+    return value.filter((item:any)=> {return item[attribute].toLowerCase().includes(filterName.toLowerCase())})
   }
 }
